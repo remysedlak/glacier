282160d9871dde963388a8d7b9e5575d5e0dde5f
remysedlak <remysedlak@gmail.com>
2026-04-12 17:52:33 -0400

    save song on exit, add better comments

--- src/audio.rs
+++ src/audio.rs
@@ -74,16 +74,14 @@ pub fn init(mut consumer: HeapCons<AudioCommand>, mut producer: HeapProd<UiComma
     let project: ProjectFile = toml::from_str(&text).unwrap();
 
     let mut bpm: f32 = project.bpm;
-
-    // track how many samples have passed since the last step
-    let mut sample_counter: f32 = 0.0;
+    let mut sample_counter: f32 = 0.0; // tracks how many samples passed, to track when a step passes
     let mut current_step = 15;
 
+    // user hardware specific
     println!("SAMPLE RATE: {}", config.sample_rate);
 
-    // set of instruments to test
+    // load a set of instruments to play
     let mut instruments: Vec<Instrument> = Vec::new();
-
     for track in project.tracks {
         instruments.push(Instrument {
             samples: path_to_vector(&track.sample_path),
@@ -98,8 +96,10 @@ pub fn init(mut consumer: HeapCons<AudioCommand>, mut producer: HeapProd<UiComma
         })
     }
 
+    // load the stored BPM onto the UI screen
     producer.try_push(UiCommand::LoadBpm(bpm)).ok();
 
+    // load each instrument individually to the UI screen
     for (i, instrument) in instruments.iter().enumerate() {
         let bools: Vec<bool> = instrument.steps.iter().map(|step| *step > 0.0).collect();
         producer
@@ -114,10 +114,11 @@ pub fn init(mut consumer: HeapCons<AudioCommand>, mut producer: HeapProd<UiComma
 
     let mut is_playing = false;
     let mut is_shutting_down = false;
-    let mut shutdown_volume: f32 = 1.00;
+    let mut shutdown_volume: f32 = 1.00; // multiplied by output data. only affects when decremented slowly to fade out audio on exit
 
     // audio callback to fill samples requested from CPAL
     let sequencer_callback = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        // before performing an audio callback, check if UI pushed any commands to pop
         while let Some(cmd) = consumer.try_pop() {
             match cmd {
                 AudioCommand::ToggleStep(x, y) => {
@@ -165,43 +166,50 @@ pub fn init(mut consumer: HeapCons<AudioCommand>, mut producer: HeapProd<UiComma
                 }
             }
         }
+        // UI commands are over; deal with the audio samples requested
+        //
+        //
 
         // for each sample requested, mix in the appropriate instrument samples
         for sample in data.chunks_mut(2) {
-            sample[0] = 0.0; // left
-            sample[1] = 0.0; // right
+            sample[0] = 0.0; // left channel
+            sample[1] = 0.0; // right channel
 
+            // for each sample, decrement the shutdown volume slowly
             if is_shutting_down {
                 shutdown_volume -= 0.0001;
                 if shutdown_volume <= 0.0 {
                     producer.try_push(UiCommand::ShutdownComplete).ok();
                 }
             }
+
+            // only if the audio is not currently paused
             if is_playing {
                 for instrument in &mut instruments {
-                    if !instrument.mute {
-                        // if the instrument is active at the current step, mix in its sample
-                        if instrument.is_playing {
-                            if instrument.position >= instrument.samples.len() as f32 {
-                                instrument.is_playing = false;
-                            } else {
-                                instrument.is_playing = true;
-                                if instrument.current_volume != instrument.target_volume {
-                                    let difference =
-                                        instrument.target_volume - instrument.current_volume;
-                                    instrument.current_volume += difference * 0.01;
-                                }
-                                // add current samples to left and right channel and increment instruments position
-                                sample[0] += instrument.samples
-                                    [(instrument.position as f32) as usize]
-                                    * instrument.current_volume
-                                    * shutdown_volume;
-                                sample[1] += instrument.samples
-                                    [(instrument.position as f32) as usize + 1]
-                                    * instrument.current_volume
-                                    * shutdown_volume;
-                                instrument.position += 2.0;
+                    // ignore muted instruments
+                    if !instrument.mute && instrument.is_playing {
+                        // if the sample fully played, mark as not playing anymore
+                        if instrument.position >= instrument.samples.len() as f32 {
+                            instrument.is_playing = false;
+                        } else {
+                            instrument.is_playing = true;
+
+                            // volume ramping
+                            if instrument.current_volume != instrument.target_volume {
+                                let difference =
+                                    instrument.target_volume - instrument.current_volume;
+                                instrument.current_volume += difference * 0.01;
                             }
+
+                            // add current samples to left and right channel and increment instruments position
+                            sample[0] += instrument.samples[(instrument.position as f32) as usize]
+                                * instrument.current_volume
+                                * shutdown_volume;
+                            sample[1] += instrument.samples
+                                [(instrument.position as f32) as usize + 1]
+                                * instrument.current_volume
+                                * shutdown_volume;
+                            instrument.position += 2.0;
                         }
                     }
                 }
