e70c3c7982e3b2d84f5a9f6e30e8573a43b955fc
remysedlak <remysedlak@gmail.com>
2026-06-07 08:33:09 -0400

    cleanup dsp crate

--- glacier-dsp/src/lib.rs
+++ glacier-dsp/src/lib.rs
@@ -1,4 +1,19 @@
-/// returns a root mean square for one window
+/// library contains implementations of ZCR, RMSE/Peaks,
+
+/// Helper. returns a peak (maximum) amplitude for one window
+///
+/// max(Σ |x|)
+/// # Arguments
+/// * samples - slice of amplitude values
+pub fn peak_window(samples: &[f32]) -> f32 {
+    let mut peak: f32 = 0.0; // accumalte the square of each sample's ampltiude
+    for sample in samples {
+        peak = f32::max(f32::abs(*sample), peak);
+    }
+    peak
+}
+
+/// Helper. returns a root mean square for one window
 ///
 /// sqrt(Σx^2 / n)
 /// # Arguments
@@ -13,11 +28,24 @@ pub fn rms_window(samples: &[f32]) -> f32 {
     rms
 }
 
-/// Root mean squared: used for volume tracking over time (db meter)
+/// helper. returns the amount of zero crosses for one window
+/// * https://www.sciencedirect.com/topics/engineering/zero-crossing-rate
+pub fn zcr_window(samples: &[f32]) -> usize {
+    let mut crosses = 0; // accumalte the square of each sample's ampltiude
+    for sample in samples.windows(2) {
+        // amplitude crosses zero (+ <-> -)
+        if (sample[0] > 0.0 && sample[1] < 0.0 || sample[0] < 0.0 && sample[1] > 0.0) && (sample[0] - sample[1]).abs() > 0.0 {
+            crosses += 1;
+        }
+    }
+    crosses
+}
+
+/// Root-Mean Square Energy: used for volume tracking over time (db meter)
 /// # Arguments
 /// * hop_size - how far apart each snapshot is taken
 /// * window_size - how wide each snapshot is
-pub fn rms(hop_size: usize, window_size: usize, samples: &[f32]) -> Vec<f32> {
+pub fn rms(samples: &[f32], window_size: usize, hop_size: usize) -> Vec<f32> {
     let mut rms_vector: Vec<f32> = vec![];
     for hop in 0..(samples.len() / hop_size) - 1 {
         rms_vector.push(rms_window(&samples[(hop * hop_size)..(hop * hop_size + window_size)]));
@@ -25,26 +53,22 @@ pub fn rms(hop_size: usize, window_size: usize, samples: &[f32]) -> Vec<f32> {
     rms_vector
 }
 
-/// returns a peak (maximum) amplitude for one window
-///
-/// max(Σ |x|)
-/// # Arguments
-/// * samples - slice of amplitude values
-pub fn peak_window(samples: &[f32]) -> f32 {
-    let mut peak: f32 = 0.0; // accumalte the square of each sample's ampltiude
-    for sample in samples {
-        peak = f32::max(f32::abs(*sample), peak);
-    }
-    peak
-}
-
-// returns a list of peak values for a list of samples
-pub fn peak(hop_size: usize, window_size: usize, samples: &[f32]) -> Vec<f32> {
+/// Peak ampltidue values
+pub fn peak(samples: &[f32], window_size: usize, hop_size: usize) -> Vec<f32> {
     let mut peak_vector: Vec<f32> = vec![];
     for hop in 0..(samples.len() / hop_size) - 1 {
         peak_vector.push(peak_window(&samples[(hop * hop_size)..(hop * hop_size + window_size)]));
     }
     peak_vector
+}
+
+/// Zero Crossing Rate
+pub fn zcr(samples: &[f32], window_size: usize, hop_size: usize) -> Vec<usize> {
+    let mut zcr_vector: Vec<usize> = vec![];
+    for hop in 0..(samples.len() / hop_size) - 1 {
+        zcr_vector.push(zcr_window(&samples[(hop * hop_size)..(hop * hop_size + window_size)]));
+    }
+    zcr_vector
 }
 
 #[cfg(test)]
@@ -70,16 +94,22 @@ mod tests {
         assert!((result - 1.0).abs() < 0.0001);
     }
     #[test]
+    fn sine_period_zcr() {
+        let samples = &sine_samples();
+        let result: usize = zcr_window(samples);
+        assert_eq!(result, 1);
+    }
+    #[test]
     fn zero_rms() {
         let zero_samples: Vec<f32> = [0.0_f32; 8192].to_vec();
-        let result: Vec<f32> = rms(512, 1024, &zero_samples);
+        let result: Vec<f32> = rms(&zero_samples, 1024, 512);
         let answer = [0.0_f32; 15];
         assert_eq!(result, answer);
     }
     pub fn sine_samples() -> Vec<f32> {
         let mut samples: Vec<f32> = vec![];
         for i in 0..1024 {
-            samples.push(f32::sin(i as f32 * (2.0 * PI / 1024.0)));
+            samples.push(f32::sin((i as f32 + 0.25) * (2.0 * PI / 1024.0)));
         }
         samples
     }
